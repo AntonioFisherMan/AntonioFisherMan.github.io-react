37103484033e98fd14604ece8b234bfef1f7e339
anton <tooxa@gmail.com>
2020-04-22 19:46:32 +0400

    commit

---
+++ client/build/precache-manifest.bc3e3dbb811393ad9e5c5c4469a07292.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2491890b43f1f9eb136bb777c8157c4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f071552a3a458a9cf9e0",
+    "url": "/static/css/3.9d9821c4.chunk.css"
+  },
+  {
+    "revision": "7d02533c7dcc03d0ab24",
+    "url": "/static/css/main.12ef1bbe.chunk.css"
+  },
+  {
+    "revision": "62863841ce44e3368b30",
+    "url": "/static/js/2.0f1e1e79.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0f1e1e79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f071552a3a458a9cf9e0",
+    "url": "/static/js/3.d881bcf9.chunk.js"
+  },
+  {
+    "revision": "7d02533c7dcc03d0ab24",
+    "url": "/static/js/main.9490d1f3.chunk.js"
+  },
+  {
+    "revision": "84a556db0dc099b4a52a",
+    "url": "/static/js/runtime-main.a41467ca.js"
+  },
+  {
+    "revision": "4ceb795a147cc8a8158cc35cc363f5f3",
+    "url": "/static/media/loader.4ceb795a.svg"
+  },
+  {
+    "revision": "f8a15e74d9b85e04019c7efd5ae76dff",
+    "url": "/static/media/user.f8a15e74.png"
+  }
+]);
